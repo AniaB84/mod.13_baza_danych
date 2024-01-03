434aa789bdc2dd5146113c0f407b0a42b261c065
AniaB84 <ania76234@gmail.com>
2024-01-03 23:58:53 +0100

    database.db

--- db.py
+++ db.py
@@ -16,6 +16,19 @@ def create_connection(db_file):
 
    return conn
 
+def create_connection_in_memory():
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(":memory:")
+       print(f"Connected, sqlite version: {sqlite3.version}")
+   except Error as e:
+       print(e)
+   finally:
+       if conn:
+           conn.close()
+
+
 def execute_sql(conn, sql):
    """ Execute sql
    :param conn: Connection object
@@ -50,29 +63,201 @@ def add_task(conn, task):
    :param task:
    :return: task id
    """
-   sql = '''INSERT INTO tasks(project_id, nazwa, opis, status, start_date, end_date)
+   sql = '''INSERT INTO tasks(projekt_id, nazwa, opis, status, start_date, end_date)
              VALUES(?,?,?,?,?,?)'''
    cur = conn.cursor()
    cur.execute(sql, task)
    conn.commit()
    return cur.lastrowid
 
+def select_task_by_status(conn, status):
+   """
+   Query tasks by priority
+   :param conn: the Connection object
+   :param status:
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM tasks WHERE status=?", (status,))
+
+   rows = cur.fetchall()
+   return rows
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+def update(conn, table, id, **kwargs):
+   """
+   update status, begin_date, and end date of a task
+   :param conn:
+   :param table: table name
+   :param id: row id
+   :return:
+   """
+   parameters = [f"{k} = ?" for k in kwargs]
+   parameters = ", ".join(parameters)
+   values = tuple(v for v in kwargs.values())
+   values += (id, )
+
+   sql = f''' UPDATE {table}
+             SET {parameters}
+             WHERE id = ?'''
+   try:
+       cur = conn.cursor()
+       cur.execute(sql, values)
+       conn.commit()
+       print("OK")
+   except sqlite3.OperationalError as e:
+       print(e)
+
+def delete_where(conn, table, **kwargs):
+   """
+   Delete from table where attributes from
+   :param conn:  Connection to the SQLite database
+   :param table: table name
+   :param kwargs: dict of attributes and values
+   :return:
+   """
+   qs = []
+   values = tuple()
+   for k, v in kwargs.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+
+   sql = f'DELETE FROM {table} WHERE {q}'
+   cur = conn.cursor()
+   cur.execute(sql, values)
+   conn.commit()
+   print("Deleted")
+
+def delete_all(conn, table):
+   """
+   Delete all rows from table
+   :param conn: Connection to the SQLite database
+   :param table: table name
+   :return:
+   """
+   sql = f'DELETE FROM {table}'
+   cur = conn.cursor()
+   cur.execute(sql)
+   conn.commit()
+   print("Deleted")
+
+   
 if __name__ == "__main__":
-   project = ("Powtórka z włoskiego", "2024-01-03 10:00:00", "2024-01-03 11:00:00")
-   conn = create_connection("database.db")
-   pr_id = add_project(conn, project)
+    create_projects_sql = """
+   -- projects table
+   CREATE TABLE IF NOT EXISTS projects (
+      id integer PRIMARY KEY,
+      nazwa text NOT NULL,
+      start_date text,
+      end_date text
+   );
+   """
+
+    create_tasks_sql = """
+   -- zadanie table
+   CREATE TABLE IF NOT EXISTS tasks (
+      id integer PRIMARY KEY,
+      projekt_id integer NOT NULL,
+      nazwa VARCHAR(250) NOT NULL,
+      opis TEXT,
+      status VARCHAR(15) NOT NULL,
+      start_date text NOT NULL,
+      end_date text NOT NULL,
+      FOREIGN KEY (projekt_id) REFERENCES projects (id)
+   );
+   """
+
+    db_file = "database.db"
+
+    conn = create_connection(db_file)
+    if conn is not None:
+       execute_sql(conn, create_projects_sql)
+       execute_sql(conn, create_tasks_sql)
+       conn.close()
+
+    create_connection_in_memory()
+    project = ("Nauka gotowania", "2024-01-04 10:00:00", "2024-01-05 11:00:00")
+    conn = create_connection("database.db")
+
+    pr_id = add_project(conn, project)
+    update(conn, "tasks", 2, status="started")
+    update(conn, "tasks", 2, stat="started")
+    conn.close()
+    delete_where(conn, "tasks", id=3)
+    delete_all(conn, "tasks")
 
 
-   task = (
-       pr_id,
-       "Słówka",
-       "Zapamiętaj słówka ze strony 200",
+    task = (
+       "6",
+       "Minestore",
+       "Strona 25",
        "started",
-       "2024-01-03 9:00:00",
-       "2024-01-03 10:50:00"
+       "2024-01-04 9:00:00",
+       "2024-01-05 10:50:00"
    )
 
-   task_id = add_task(conn, task)
+    task_id = add_task(conn, task)
 
-   print(pr_id, task_id)
-   conn.commit()
+    print(pr_id, task_id)
+    conn.commit()
+
+    
+    conn = create_connection("database.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+    <sqlite3.Cursor at 0x11088d5e0>
+    rows = cur.fetchall()
+    rows
+    [(1,
+  6,
+  'Czasowniki regularne',
+  'Zapamiętaj czasowniki ze strony 30',
+  'started',
+  '2020-05-11 12:00:00',
+  '2020-05-11 15:00:00'),
+ (2,
+  7,
+  'Czasowniki regularne',
+  'Zapamiętaj czasowniki ze strony 30',
+  'started',
+  '2020-05-11 12:00:00',
+  '2020-05-11 15:00:00'),
+ (3,
+  8,
+  'Czasowniki regularne',
+  'Zapamiętaj czasowniki ze strony 30',
+  'started',
+  '2020-05-11 12:00:00',
+  '2020-05-11 15:00:00')]
+
